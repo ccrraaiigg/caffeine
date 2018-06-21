416fb6f9a5ff24438fe38d4fc07bf9c09b8f345f
Craig Latta <github@netjam.org>
2018-06-21 15:56:44 +0200

    graceful display of VR-mode and home buttons

--- js/a-frame/craig/mobileUtilities.js
+++ js/a-frame/craig/mobileUtilities.js
@@ -364,11 +364,3 @@ document.body.addEventListener(
       false)},
   false)
 
-var html = document.getElementsByTagName('HTML')
-
-html[0].style.position = 'initial'
-html[0].style.overflow = 'scroll'
-
-var canvas = document.querySelector('.a-canvas')
-canvas.style.top = '1000px'
-
