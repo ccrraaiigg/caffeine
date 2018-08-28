8c7e16caae7c0a8c8d16e257261cd2757e12aa77
Craig Latta <github@netjam.org>
2018-08-28 16:59:38 +0200

    Add HTML Imports polyfill for Safari.

--- js/impress/plugins/touch.js
+++ js/impress/plugins/touch.js
@@ -23,46 +23,48 @@
     var lastDX = 0;
     var threshold = window.innerWidth / 20;
 
-    document.addEventListener( "touchstart", function( event ) {
+  document.addEventListener( "touchstart", function( event ) {
+    event.preventDefault();
         lastX = startX = event.touches[ 0 ].clientX;
     } );
 
-    document.addEventListener( "touchmove", function( event ) {
-         var x = event.touches[ 0 ].clientX;
-         var diff = x - startX;
+  document.addEventListener( "touchmove", function( event ) {
+    event.preventDefault();
+    var x = event.touches[ 0 ].clientX;
+    var diff = x - startX;
 
-         // To be used in touchend
-         lastDX = lastX - x;
-         lastX = x;
+    // To be used in touchend
+    lastDX = lastX - x;
+    lastX = x;
 
-         window.impress().swipe( diff / window.innerWidth );
-     } );
+    window.impress().swipe( diff / window.innerWidth );
+  } );
 
-     document.addEventListener( "touchend", function() {
-         var totalDiff = lastX - startX;
-         if ( Math.abs( totalDiff ) > window.innerWidth / 5 && ( totalDiff * lastDX ) <= 0 ) {
-             if ( totalDiff > window.innerWidth / 5 && lastDX <= 0 ) {
-                 window.impress().prev();
-             } else if ( totalDiff < -window.innerWidth / 5 && lastDX >= 0 ) {
-                 window.impress().next();
-             }
-         } else if ( Math.abs( lastDX ) > threshold ) {
-             if ( lastDX < -threshold ) {
-                 window.impress().prev();
-             } else if ( lastDX > threshold ) {
-                 window.impress().next();
-             }
-         } else {
+  document.addEventListener( "touchend", function() {
+    var totalDiff = lastX - startX;
+    if ( Math.abs( totalDiff ) > window.innerWidth / 5 && ( totalDiff * lastDX ) <= 0 ) {
+      if ( totalDiff > window.innerWidth / 5 && lastDX <= 0 ) {
+        window.impress().prev();
+      } else if ( totalDiff < -window.innerWidth / 5 && lastDX >= 0 ) {
+        window.impress().next();
+      }
+    } else if ( Math.abs( lastDX ) > threshold ) {
+      if ( lastDX < -threshold ) {
+        window.impress().prev();
+      } else if ( lastDX > threshold ) {
+        window.impress().next();
+      }
+    } else {
 
-             // No movement - move (back) to the current slide
-             window.impress().goto( document.querySelector( "#impress .step.active" ) );
-         }
-     } );
+      // No movement - move (back) to the current slide
+      window.impress().goto( document.querySelector( "#impress .step.active" ) );
+    }
+  } );
 
-     document.addEventListener( "touchcancel", function() {
+  document.addEventListener( "touchcancel", function() {
 
-             // Move (back) to the current slide
-             window.impress().goto( document.querySelector( "#impress .step.active" ) );
-     } );
+    // Move (back) to the current slide
+    window.impress().goto( document.querySelector( "#impress .step.active" ) );
+  } );
 
 } )( document, window );
--- js/talk.js
+++ js/talk.js
@@ -1,58 +1,61 @@
-var wireframes = document.querySelector('link[rel="import"]').import,
-    workspace = wireframes.getElementById('workspace'),
-    workspaceSlideNames = [
-      'parsing-javascript',
-      'speech-recognition',
-      'webmidi',
-      'chrome-debugging-protocol'],
-    canvasNames = [
-      'title-workspace',
-      'morphicjs-window',
-      'snowglobe-demo-window',
-      'parsing-javascript-workspace',
-      'speech-recognition-workspace',
-      'webmidi-workspace',
-      'chrome-debugging-protocol-workspace'],
-    editorNames = [
-      'text-editor-widget',
-      'a-frame']
-
-document.getElementById('a-frame').addEventListener(
-  'mouseleave',
-  (event) => {
-    document.getElementById('a-frame').blur()})
-
 window.addEventListener(
-  'keydown',
-  (event) => {
-    document.body.parentNode.style.cursor = 'none'
-    if (event.key === 'c') {
-      if (document.activeElement === document.body) impress().goto('caffeine')}
-    if (event.key === 'o') {
-      if (document.activeElement === document.body) impress().goto('overview')}})
+  'WebComponentsReady',
+  () => {
+    var wireframes = document.querySelector('link[rel="import"]').import,
+	workspace = wireframes.getElementById('workspace'),
+	workspaceSlideNames = [
+	  'parsing-javascript',
+	  'speech-recognition',
+	  'webmidi',
+	  'chrome-debugging-protocol'],
+	canvasNames = [
+	  'title-workspace',
+	  'morphicjs-window',
+	  'snowglobe-demo-window',
+	  'parsing-javascript-workspace',
+	  'speech-recognition-workspace',
+	  'webmidi-workspace',
+	  'chrome-debugging-protocol-workspace'],
+	editorNames = [
+	  'text-editor-widget',
+	  'a-frame']
 
-window.addEventListener(
-  'mousemove',
-  (event) => {document.body.parentNode.style.cursor = ''})
+    document.getElementById('a-frame').addEventListener(
+      'mouseleave',
+      (event) => {
+	document.getElementById('a-frame').blur()})
 
-window.addEventListener(
-  'mousedown',
-  (event) => {document.body.parentNode.style.cursor = ''})
+    window.addEventListener(
+      'keydown',
+      (event) => {
+	document.body.parentNode.style.cursor = 'none'
+	if (event.key === 'c') {
+	  if (document.activeElement === document.body) impress().goto('caffeine')}
+	if (event.key === 'o') {
+	  if (document.activeElement === document.body) impress().goto('overview')}})
 
-document.getElementById('code-browser-wireframe').appendChild(wireframes.getElementById('classes-browser').cloneNode(true))
+    window.addEventListener(
+      'mousemove',
+      (event) => {document.body.parentNode.style.cursor = ''})
 
-workspaceSlideNames.forEach((id) => {
-  var slideWorkspace = workspace.cloneNode(true)
+    window.addEventListener(
+      'mousedown',
+      (event) => {document.body.parentNode.style.cursor = ''})
 
-  slideWorkspace.id = id + '-' + slideWorkspace.id
-  document.getElementById(id).appendChild(slideWorkspace)})
+    document.getElementById('code-browser-wireframe').appendChild(wireframes.getElementById('classes-browser').cloneNode(true))
 
-canvasNames.forEach((id) => {window.makeCanvasEditable(document.getElementById(id))})
-editorNames.forEach((id) => {window.makeEditorEditable(document.getElementById(id))})
+    workspaceSlideNames.forEach((id) => {
+      var slideWorkspace = workspace.cloneNode(true)
 
-window.setImpressDuration = (string) => {
-  var divs = document.getElementById('impress').children[0].children
-  
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].dataset.transitionDuration = string}}
+      slideWorkspace.id = id + '-' + slideWorkspace.id
+      document.getElementById(id).appendChild(slideWorkspace)})
+
+    canvasNames.forEach((id) => {window.makeCanvasEditable(document.getElementById(id))})
+    editorNames.forEach((id) => {window.makeEditorEditable(document.getElementById(id))})
+
+    window.setImpressDuration = (string) => {
+      var divs = document.getElementById('impress').children[0].children
+      
+      for (var i = 0; i < divs.length; i++) {
+	divs[i].dataset.transitionDuration = string}}})
 
