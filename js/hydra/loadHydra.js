1e6b1cd54e8f011cf70554a1a01f25673b48cc8b
Craig Latta <github@netjam.org>
2019-09-12 23:43:45 +0200

    .

--- js/hydra/loadHydra.js
+++
@@ -1,6 +0,0 @@
-// Load Hydra into a web browser window (build a loadable standalone bundle
-// from this with browserify).
-
-const Hydra = require('./node_modules/hydra-synth')
-window.Hydra = Hydra
-
