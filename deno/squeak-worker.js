206af4d7501f5628c2f81315431f13ce87e5eac6
Craig Latta <github@netjam.org>
2024-11-06 18:07:37 -0800

    xx

--- deno/squeak-worker.js
+++ deno/squeak-worker.js
@@ -18,7 +18,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- */
+ */ 
 
 
 // Set up an environment for running the SqueakJS VM headlessly in a Web Worker.
