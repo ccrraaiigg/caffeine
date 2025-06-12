d86e1735fc6dd46a26b2f9debb67837f2a8226f9
Craig Latta <github@netjam.org>
2025-06-11 19:51:51 -0700

    typoo

--- js/squeakjs/plugins/BitBltPlugin.js
+++ js/squeakjs/plugins/BitBltPlugin.js
@@ -5,7 +5,7 @@
 	BitBltSimulation VMMaker-bf.353 uuid: 8ae25e7e-8d2c-451e-8277-598b30e9c002
  */
 
-if (!window) const window = globalThis
+if (!window) {window = globalThis}
 window.module("users.bert.SqueakJS.plugins.BitBltPlugin").requires("users.bert.SqueakJS.vm").toRun(function() {
 "use strict";
 
